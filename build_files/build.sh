fb4c4d62d33f0120c6c723a938f605f237786666
Espen Kristiansen <espen@appresso.no>
2025-08-03 22:55:40 +0200

    Initial setup

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,55 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+
+dnf5 groupinstall -y "Development Tools" "Development Libraries"
+
+dnf5 install -y wl-clipboard \
+    tmux \
+    gcc-c++ \
+    dotnet-sdk-8.0 \
+    openssl \
+    bat \
+    lastpass-cli \
+    helm kubernetes-client \
+    readline-devel \
+    cmake \
+    python3-pip python3-devel pipx \
+    redhat-rpm-config \
+    fzf ripgrep atuin \
+    libtiff-devel libjpeg-devel openjpeg2-devel zlib-devel \
+    freetype-devel lcms2-devel libwebp-devel tcl-devel tk-devel \
+    harfbuzz-devel fribidi-devel libraqm-devel libimagequant-devel libxcb-devel \
+    gdal-devel gdal-libs \
+    pango libffi-devel \
+    libpq-devel postgresql \
+    ghostscript \
+    azure-cli \
+    dnsmasq \
+    sassc \
+    sqlite sqlite-devel \
+    git-delta \
+    docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \
+    kitty
+
+
+cat > /etc/ssh/sshd_config.d/99-custom.conf << EOF
+# Disable password authentication
+PasswordAuthentication no
+
+# Disable root login
+PermitRootLogin no
+
+# Disable X11 forwarding (unless you need it)
+X11Forwarding no
+
+# Disable TCP forwarding (unless you need it)
+AllowTcpForwarding no
+
+# Enable pubke authentication
+PubkeyAuthentication yes
+EOF
 
 # Use a COPR Example:
 #
@@ -21,4 +68,4 @@ dnf5 install -y tmux
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
